1912b40dd8fc2c43fd5720ca5ee62f8d108405ad
bigelowcc <liam@cloudcannon.com>
2020-10-03 21:45:01 +1300

    Adding lighthouse component

---
+++ component-library/components/lighter/lighter.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let big = false;
+	export let lit = false;
+	export let title = "";
+
+	const light = () => {
+		lit = !lit;
+	}
+</script>
+
+<div class="c-lighter"
+	 class:is-svelte={true}
+	 class:c-lighter--big={big}
+	 class:is-lit={lit}
+	 on:click={light}>
+
+    <div class="c-lighter__title">
+    	{ title }
+    </div>
+
+</div>
